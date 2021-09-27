265c552ffdd7b1a8fc0ffc61e995c5236820c496
dawidWejman <78164771+dawidWejman@users.noreply.github.com>
2021-09-27 14:27:36 +0100

    Update cars.js

--- cars.js
+++ cars.js
@@ -3,9 +3,9 @@ class vechicle
     constructor(make,moodel,year)
     {
         
-        this.make = make;
-        this.moodel = model;
-        this.year = year;
+        this.make = make;//veriable to make
+        this.moodel = model;//veriable for model 
+        this.year = year;//veriable for year
     }
     informations(){
         console.log('make: ${this.make}');
@@ -20,13 +20,13 @@ class car extends vehicle
 {
     constructor(make, model, year, doors)
     {
-        super(make, model,year);
+        super(make, model,year);//super method just to transform veriables from dif class
         this.doors = doors;
     }
     informations(){
         super.informations();
-        console.log('number of doors: ${this.doors}');
+        console.log('number of doors: ${this.doors}');//console for maing numbers of doors
     }
 }
-let myCar = new cars ('VW', 'golf', '2021', '4');
-let info = myCar.informations();
+let myCar = new cars ('VW', 'golf', '2021', '4');//creating file
+let info = myCar.informations();//main log
